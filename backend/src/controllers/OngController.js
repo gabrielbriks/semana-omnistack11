2ab1d32e72d55252f304be678b2f63b7055da181
gabrielbriks <greis9010@gmail.com>
2020-03-29 00:01:12 -0300

    Adicionando o Ongcontroller

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,30 @@
+const connection = require("../database/conection");
+const crypto = require("crypto");
+
+module.exports = {
+  async index(req, res) {
+    const ongs = await connection("ongs").select("*");
+
+    return res.json(ongs);
+  },
+
+  async create(req, res) {
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    /*
+      Gerando 'id' aleatorio de 4 caracteres.  
+    */
+    const id = crypto.randomBytes(4).toString("HEX");
+
+    await connection("ongs").insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    });
+
+    return res.json({ id });
+  }
+};
