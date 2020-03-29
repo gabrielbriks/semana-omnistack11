08158fc6fae4cbee8df7b705caf80113487b9fff
gabrielbriks <greis9010@gmail.com>
2020-03-29 00:00:35 -0300

    Adicionando as rotas da aplicação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,28 @@
 const express = require("express");
-const crypto = require("crypto");
+
+const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
-routes.post("/ongs", (req, res) => {
-  const { name, email, whatsapp, city, uf } = req.body;
+// Login
+routes.post("/sessions", SessionController.create);
 
-  console.log(body);
+//List ongs
+routes.get("/ongs", OngController.index);
+//Register ongs
+routes.post("/ongs", OngController.create);
 
-  return res.json();
-});
+//Get casos especificos de uma ONG
+routes.get("/profile", ProfileController.index);
+
+//List Incidents
+routes.get("/incidents", IncidentController.index);
+//Register Incidents
+routes.post("/incidents", IncidentController.create);
+//Delete Incidents
+routes.delete("/incidents/:id", IncidentController.delete);
 
 module.exports = routes;
