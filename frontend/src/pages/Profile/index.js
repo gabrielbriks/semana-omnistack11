4e9d3d753f18aafba5275a06510bcc43c9c377d7
gabrielbriks <greis9010@gmail.com>
2020-04-12 14:26:46 -0300

    Page Profile - Implementa nome da Ong dinâmicamente.
    Utilizando o nome da ong dinâmicamente buscando do localStorage.

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FiPower, FiTrash2 } from "react-icons/fi";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
 
-import "./styles.css";
-import logoImg from "../../assets/logo.svg";
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
+  const ongName = localStorage.getItem('ongName');
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be The Hero" />
-        <span>Bem vinda, APAD</span>
+        <span>Bem vinda, {ongName}</span>
         <Link className="button" to="/incidents/new">
           Cadastrar novo caso
         </Link>
