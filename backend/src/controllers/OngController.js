fc3bc25ca77524bbb09facfcc15287ac4127b379
gabrielbriks <greis9010@gmail.com>
2020-04-12 13:48:14 -0300

    Backend - Adiciona ajustes no padrao do código
    Foi a padronizado algumas abordagens no código automaticamento com o
    prettier e com o eslint.

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,9 +1,9 @@
-const connection = require("../database/conection");
-const crypto = require("crypto");
+const connection = require('../database/conection');
+const crypto = require('crypto');
 
 module.exports = {
   async index(req, res) {
-    const ongs = await connection("ongs").select("*");
+    const ongs = await connection('ongs').select('*');
 
     return res.json(ongs);
   },
@@ -14,17 +14,17 @@ module.exports = {
     /*
       Gerando 'id' aleatorio de 4 caracteres.  
     */
-    const id = crypto.randomBytes(4).toString("HEX");
+    const id = crypto.randomBytes(4).toString('HEX');
 
-    await connection("ongs").insert({
+    await connection('ongs').insert({
       id,
       name,
       email,
       whatsapp,
       city,
-      uf
+      uf,
     });
 
     return res.json({ id });
-  }
+  },
 };
