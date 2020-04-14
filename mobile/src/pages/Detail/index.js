93d292c07aa9a9f8e1ed6aa656fc9bcde5354175
gabrielbriks <greis9010@gmail.com>
2020-04-13 23:05:26 -0300

    Adiciona o Linking para acessar o whats
    Utilizando uma biblioteca do próprio react-native chamada de "Linking" para abrir o app do whatsapp

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
-import { View, TouchableOpacity, Image, Text } from 'react-native';
+import { View, TouchableOpacity, Image, Text, Linking } from 'react-native';
 import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
@@ -24,7 +24,9 @@ export default function Detail() {
     });
   }
 
-  function sendWhatsapp() {}
+  function sendWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=+5561984849709&text=${message}`);
+  }
 
   return (
     <View style={styles.container}>
@@ -54,7 +56,7 @@ export default function Detail() {
         <Text style={styles.heroDescription}>Entre em contato:</Text>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
             <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
 
