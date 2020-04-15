895edd53c8645ba339090b9578aebe3ce968b417
gabrielbriks <greis9010@gmail.com>
2020-04-14 22:57:09 -0300

    Mobile - Implementa listagens de Incidents dinâmico
    Implementando exibição de Incidents cadastrados por uma Ong

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 export default function Incidents() {
+  const [incidents, setIncidents] = useState([]);
+  const [total, setTotal] = useState(0);
   const navigation = useNavigation();
 
-  function navigationToDetail() {
-    navigation.navigate('Detail');
+  function navigationToDetail(incident) {
+    navigation.navigate('Detail', { incident });
   }
+
+  async function loadIncident() {
+    const response = await api.get('incidents');
+
+    setIncidents(response.data);
+    setTotal(response.headers['x-total-count']);
+  }
+
+  useEffect(() => {
+    loadIncident();
+  }, []);
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={logoImg} />
         <Text style={styles.headerText}>
-          Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+          Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
         </Text>
       </View>
 
@@ -28,24 +42,28 @@ export default function Incidents() {
       </Text>
 
       <FlatList
-        data={[1, 2, 3]}
+        data={incidents}
         style={styles.incidentList}
-        keyExtractor={incident => String(incident)}
+        keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false} /*ocultando barra de rolagem*/
-        renderItem={() => (
+        renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
-            <Text style={styles.incidentValue}>APAD:</Text>
-
+            <Text style={styles.incidentValue}>{incident.name}</Text>
             <Text style={styles.incidentProperty}>CASO:</Text>
-            <Text style={styles.incidentValue}>Cadelinha atropelada:</Text>
-
+            <Text style={styles.incidentValue}>{incident.title}</Text>
             <Text style={styles.incidentProperty}>VALOR:</Text>
-            <Text style={styles.incidentValue}>R$ 120,00</Text>
-
+            <Text style={styles.incidentValue}>
+              {Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(incident.value)}
+            </Text>
             <TouchableOpacity
               style={styles.detailsButton}
-              onPress={navigationToDetail}
+              onPress={() => {
+                navigationToDetail(incident);
+              }}
             >
               <Text style={styles.detailsButtonText}>Ver Mais Detalhes</Text>
               <Feather name="arrow-right" size={16} color="#e02041" />
