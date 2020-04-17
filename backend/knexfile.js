9f2f59dc4df56bf40045b3afb4333b42698d5a04
gabrielbriks <greis9010@gmail.com>
2020-04-16 21:17:56 -0300

    Adiciona conexão de banco teste para o jest

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,48 +2,60 @@
 
 module.exports = {
   development: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: "./src/database/db.sqlite"
+      filename: './src/database/db.sqlite',
     },
     migrations: {
-      directory: "./src/database/migrations"
+      directory: './src/database/migrations',
     },
     /* como o sqlite não suporta que os campos teja valores padroes, utilizamos essa
       flag, que vaí possibilitar que existe campos com valores nulos em nossa table.
     */
-    useNullAsDefault: true
+    useNullAsDefault: true,
+  },
+
+  //Conexão de banco de teste com jest
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/test.sqlite',
+    },
+    migrations: {
+      directory: './src/database/migrations',
+    },
+    useNullAsDefault: true,
   },
 
   staging: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: "knex_migrations"
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: "knex_migrations"
-    }
-  }
+      tableName: 'knex_migrations',
+    },
+  },
 };
